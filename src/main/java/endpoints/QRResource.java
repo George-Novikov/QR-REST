a8d2622fa15d99f03405de55fe24cd06ab96e5f9
FateMorgan <fatemorgan@yandex.com>
2023-04-26 21:07:29 +0600

    Implemented a proper response encoding

--- src/main/java/endpoints/QRResource.java
+++ src/main/java/endpoints/QRResource.java
@@ -5,7 +5,6 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import tools.*;
 
 import javax.imageio.ImageIO;
-import javax.print.attribute.standard.Media;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -23,7 +22,7 @@ public class QRResource {
     @POST
     @Path("/pdf")
     @Consumes("application/pdf")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON + "; charset=utf-8")
     public Response readPDF(InputStream input){
         int errorCode = 0;
         List<String> data = new ArrayList();
@@ -59,7 +58,7 @@ public class QRResource {
                     }
                 }
 
-                result = StringEncoder.encode(result);
+                //result = StringEncoder.encode(result);
 
                 if (result.length() > 0) {
                     data.add(result);
@@ -81,7 +80,7 @@ public class QRResource {
     @POST
     @Path("/image")
     @Consumes({"image/jpg", "image/png", "image/gif", "image/svg+xml", "image/webp", "image/avif", "image/apng"})
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON + "; charset=utf-8")
     public Response readImage(InputStream input){
         int errorCode = 0;
         List<String> data = new ArrayList();
@@ -97,7 +96,7 @@ public class QRResource {
             BufferedInputStream bufferStream = new BufferedInputStream(input);
             BufferedImage bufferedImage = ImageIO.read(bufferStream);
             result = QRReader.readQR(bufferedImage, charset, hashMap);
-            result = StringEncoder.encode(result);
+            //result = StringEncoder.encode(result);
             data.add(result);
         } catch (Throwable e){
             errorCode = 2;
@@ -113,7 +112,7 @@ public class QRResource {
 
     @POST
     @Path("/create")
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON + "; charset=utf-8")
     @Produces("image/png")
     public Response createQR(CreateRequest request){
         String charset = "UTF-8";
@@ -134,7 +133,7 @@ public class QRResource {
     @POST
     @Path("/size")
     @Consumes({"image/jpg", "image/png", "image/gif", "image/svg+xml", "image/webp", "image/avif", "image/apng"})
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON + "; charset=utf-8")
     public Response readImageSize(InputStream input){
         int errorCode = 0;
         List<String> data = new ArrayList();
@@ -161,7 +160,7 @@ public class QRResource {
     @POST
     @Path("/pagessize")
     @Consumes("application/pdf")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON + "; charset=utf-8")
     public Response readPDFSize(InputStream input){
         int errorCode = 0;
         List<String> data = new ArrayList();
