54e28a67053eac4e0b7551ae4b69cefd9170d578
FateMorgan <fatemorgan@yandex.com>
2023-04-26 21:24:13 +0600

    Refactored resources into a more logical form

---
+++ src/main/java/endpoints/EncodingResource.java
@@ -0,0 +1,34 @@
+package endpoints;
+
+import tools.CreateRequest;
+import tools.QRReader;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/encode")
+public class EncodingResource {
+    @POST
+    @Path("/create")
+    @Consumes(MediaType.APPLICATION_JSON + "; charset=utf-8")
+    @Produces("image/png")
+    public Response createQR(CreateRequest request){
+        String charset = "UTF-8";
+        int httpStatus = 200;
+
+        byte[] byteResponse = null;
+
+        try {
+            byteResponse = QRReader.createQR(request.data, charset, request.size);
+        } catch (Throwable e){
+            httpStatus = 500;
+        }
+
+        Response.ResponseBuilder builder = Response.status(httpStatus).entity(byteResponse);
+        return builder.build();
+    }
+}
